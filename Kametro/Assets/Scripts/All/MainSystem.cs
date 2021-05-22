cb36c1de277cf7dea3978c8b35fb3efafb3de10f
ryuta2727 <ryuta7260@icloud.com>
2021-05-22 22:01:12 +0900

    ryuta2727
    ブロック仕様

---
+++ Kametro/Assets/Scripts/All/MainSystem.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// ゲームのすべての管理
+/// </summary>
+public abstract class MainSystem : MonoBehaviour
+{
+    //---public-------------------------------------------------------
+
+
+    //---private------------------------------------------------------
+
+
+    //---protected----------------------------------------------------
+    /// <summary>
+    /// 初期化
+    /// </summary>
+    protected abstract void Init();
+
+    /// <summary>
+    /// メイン処理
+    /// </summary>
+    protected abstract void Main();
+
+    /// <summary>
+    /// UI以外の描画
+    /// </summary>
+    protected abstract void Draw();
+
+    /// <summary>
+    /// UI描画
+    /// </summary>
+    protected abstract void UISystem();
+
+    /// <summary>
+    /// 破棄
+    /// </summary>
+    protected abstract void Dest();
+}
